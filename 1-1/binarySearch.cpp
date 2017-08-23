eb21b4b51f08ecfd842b73531cf118a6024a8463
xiulwang <xiulwang@users.noreply.github.com>
2017-08-23 18:06:00 +0800

    Update binarySearch.cpp

--- 1-1/binarySearch.cpp
+++ 1-1/binarySearch.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
+
+void read(string input_text, vector<int>&inputNum)
+{
+	ifstream in(input_text);
+	int num;
+	while (in>>num){
+		inputNum.push_back(num);
+	}
+}
+
+
+int binarySearch(int key, vector<int>inputNum)
+{
+	vector<int>::size_type left = 0;
+	auto right = inputNum.size() - 1;
+	while (left <= right){
+		auto mid = (right - left) / 2+left;
+		if (key == inputNum[mid])
+			return mid;
+		else if (key < inputNum[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return -1;
+}
+
+int main()
+{
+	vector<int>inputNum;
+	read("input.txt",inputNum);
+	sort(inputNum.begin(), inputNum.end());
+	for (auto i : inputNum){
+		cout << i << endl;
+	}
+	int key;
+	cin >> key;
+	int ind = binarySearch(key, inputNum);
+	cout << ind;
+	system("pause");
+	return 0;
+}
